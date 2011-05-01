2e625e259845d28e00d0795efddc02b134602043
Pierre Hellwig <pierre.hellwig@gmail.com>
2011-05-01 22:57:30 +0200

    added help

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -55,7 +55,7 @@ describe PagesController do
     end
     
     it "should have the right title" do
-      get 'about'
+      get 'help'
       response.should have_selector("title",
                         :content =>
                           @base_title + " | Help")
